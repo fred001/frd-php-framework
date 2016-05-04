ac2eee9c86fd73350908336f6bc371c7515c2b49
frd <frd@iamlosing.me>
2016-05-04 11:49:15 +0800

    update document

---
+++ doc/english/4.api/1.index.php
@@ -0,0 +1,6 @@
+=========================index.php=======================
+Location: 
+  public/index.php
+
+Explanation:
+  please look at this file's code comments
--- public/index.php
+++ public/index.php
@@ -1,46 +1,59 @@
 <?php
-ini_set("display_errors",1);
-$LIB_PATH=dirname(__FILE__)."/../lib";
-set_include_path($LIB_PATH.PATH_SEPARATOR.get_include_path());
+   /*
+   what does this file did ?
 
-require_once("Frd/Frd.php");
-define("ROOT_PATH",dirname(__FILE__).'/../');
-require_once(ROOT_PATH."/functions.php");
+   1.add lib folder to include_path (for autoloader)
+   2.define  ROOT_PATH  (for other place use this variable)
+   3.load setting
+   4.Frd::init($setting)   (init)
+   5.app()->run()   (after init, the Frd_App object created ,so not can run )
+   */
 
-//read config
-require_once(ROOT_PATH."/default_setting.php");
+   ini_set("display_errors",1);
+   $LIB_PATH=dirname(__FILE__)."/../lib";
+   set_include_path($LIB_PATH.PATH_SEPARATOR.get_include_path());
+   require_once("Frd/Frd.php");  //load core class 
 
-if(file_exists(ROOT_PATH."/local/setting.php"))
-{
-  require_once(ROOT_PATH."/local/setting.php");
+   define("ROOT_PATH",dirname(__FILE__).'/../');
+   
+   //this file include framework's interface function 
+   //such as    app(), url() ,getModule()
+   require_once(ROOT_PATH."/functions.php"); 
 
-  $setting=array_merge($setting_default,$setting);
-}
-else
-{
-  $setting=$setting_default;
-}
+   //read config
+   require_once(ROOT_PATH."/default_setting.php");
+
+   if(file_exists(ROOT_PATH."/local/setting.php"))
+   {
+      require_once(ROOT_PATH."/local/setting.php");
+
+      $setting=array_merge($setting_default,$setting);
+   }
+   else
+   {
+      $setting=$setting_default;
+   }
 
 
-Frd::init($setting);
-session_start();
+   Frd::init($setting);
+   session_start();
 
-try{
-  app()->run();
-}
-catch(Exception $e)
-{
-  if($e->getMessage() == "REWRITE URL FAILED")
-  {
-     app()->run("/404");
+   try{
+      //will parse url and load correct controller 
+      app()->run();
+   }
+   catch(Exception $e)
+   {
+      if($e->getMessage() == "REWRITE URL FAILED")
+      {
+         app()->run("/404");
 
-    //require_once(ROOT_PATH."/error/404.php");
-  }
-  else
-  {
-     throw $e;
-    //error_log($e);
-    //app()->run("/error");
-    //require_once(ROOT_PATH."/error/error.php");
-  }
-}
+         //require_once(ROOT_PATH."/error/404.php");
+      }
+      else
+      {
+         throw $e;
+         //error_log($e);
+         //app()->run("/error");
+      }
+   }
