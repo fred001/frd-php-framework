73107c30c52c5cdebccacdc28e6b0cc27ca38811
renyj <renyj@anchu.com>
2016-12-09 09:11:49 +0800

    update

--- modules/default/controller/index.php
+++ modules/default/controller/index.php
@@ -1,20 +1,4 @@
 <?php
-
-   $table=new Frd_Db_Table("message");
-   $api=new Frd_Api($table);
-
-   //$data=$api->find_all();
-
-   $params=array('page'=>1,'page_count'=>2);
-   $data=$api->find($params);
-
-
-   print_r($data);
-
-   echo 'aa';exit();
-
-
-
 $layout=getModule()->getLayout("basic");
 $layout->content=getModule()->render("index",array(
 ));
